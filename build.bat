94640372c799b90e2068feee414f854823a2a522
Axe Barbie <ehavahrt@gmail.com>
2024-01-07 19:03:38 -0500

    Site updated

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+
+cd src && foto export -m -o ..\live && cd ..\
+cd src\boys-axes && foto export -m -o ..\..\live\boys-axes && cd ..\..\
+cd src\double-bits && foto export -m -o ..\..\live\double-bits && cd ..\..\
+cd src\hatchets && foto export -m -o ..\..\live\hatchets && cd ..\..\
+cd src\single-bits && foto export -m -o ..\..\live\single-bits && cd ..\..\
