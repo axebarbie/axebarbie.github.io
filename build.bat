5bad76b0477607d9d66fb4110a62fbc871d46c43
Axe Barbie <ehavahrt@gmail.com>
2024-01-24 18:14:23 -0500

    Sears sabre saw & kelly hammer hatchet added

--- build.bat
+++ build.bat
@@ -5,3 +5,4 @@ cd src\boys-axes && foto export -m -o ..\..\live\boys-axes && cd ..\..\
 cd src\double-bits && foto export -m -o ..\..\live\double-bits && cd ..\..\
 cd src\hatchets && foto export -m -o ..\..\live\hatchets && cd ..\..\
 cd src\single-bits && foto export -m -o ..\..\live\single-bits && cd ..\..\
+cd src\power-tools && foto export -m -o ..\..\live\power-tools && cd ..\..\
