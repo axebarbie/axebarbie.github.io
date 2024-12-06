a5edef2417c3b499c05bcb6099d6d8f81cbb8fae
Axe Barbie <ehavahrt@gmail.com>
2024-12-06 18:16:20 -0500

    Banjo tabs added

--- build.bat
+++ build.bat
@@ -1,11 +1,12 @@
 @echo off
 
-cd src && foto export -m -o ..\live && cd ..\
-cd src\boys-axes && foto export -m -o ..\..\live\boys-axes && cd ..\..\
-cd src\double-bits && foto export -m -o ..\..\live\double-bits && cd ..\..\
-cd src\hatchets && foto export -m -o ..\..\live\hatchets && cd ..\..\
-cd src\single-bits && foto export -m -o ..\..\live\single-bits && cd ..\..\
-cd src\power-tools && foto export -m -o ..\..\live\power-tools && cd ..\..\
-cd src\catalogs && foto export -m -o ..\..\live\catalogs && cd ..\..\
+cd src && ..\foto.exe export -m -o ..\live && cd ..\
+cd src\boys-axes && ..\..\foto.exe export -m -o ..\..\live\boys-axes && cd ..\..\
+cd src\double-bits && ..\..\foto.exe export -m -o ..\..\live\double-bits && cd ..\..\
+cd src\hatchets && ..\..\foto.exe export -m -o ..\..\live\hatchets && cd ..\..\
+cd src\single-bits && ..\..\foto.exe export -m -o ..\..\live\single-bits && cd ..\..\
+cd src\power-tools && ..\..\foto.exe export -m -o ..\..\live\power-tools && cd ..\..\
+cd src\catalogs && ..\..\foto.exe export -m -o ..\..\live\catalogs && cd ..\..\
+cd src\banjo && ..\..\foto.exe export -m -o ..\..\live\banjo && cd ..\..\
 
 pause
