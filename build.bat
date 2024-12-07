01f2aa45cb7c1f63226f342df3ceada82a09980e
Axe Barbie <ehavahrt@gmail.com>
2024-12-07 15:44:21 -0500

    Tabs removed

--- build.bat
+++ build.bat
@@ -7,6 +7,5 @@ cd src\hatchets && ..\..\foto.exe export -m -o ..\..\live\hatchets && cd ..\..\
 cd src\single-bits && ..\..\foto.exe export -m -o ..\..\live\single-bits && cd ..\..\
 cd src\power-tools && ..\..\foto.exe export -m -o ..\..\live\power-tools && cd ..\..\
 cd src\catalogs && ..\..\foto.exe export -m -o ..\..\live\catalogs && cd ..\..\
-cd src\banjo && ..\..\foto.exe export -m -o ..\..\live\banjo && cd ..\..\
 
 pause
